186cbc54b3a652526ebe189aebf8ba7fe245ab48
Alex Kirikov <alex.kirikov@gmail.com>
2025-11-15 10:09:03 +0200

    add task1 - fibonacci

---
+++ data/task1.py
@@ -0,0 +1,21 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+
+# ======== Тести ========
+fib = caching_fibonacci()
+print(fib(10))  # має вивести 55
+print(fib(15))  # має вивести 610
